18a18f8f1e7c714b7221a1312083221d12f6a07c
muharrem gem <muharremgem28@gmail.com>
2022-12-09 17:40:17 +0300

    images next

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -17,15 +18,26 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <img
-        className="relative rounded-full h-32 w-32 mx-auto object-cover"
-        src="https://pbs.twimg.com/profile_images/1447671101530775553/PpbiCpy7_400x400.jpg"
-        alt=""
-      />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div className="relative h-32 w-32 mx-auto">
+        <Image
+          src={
+            "https://pbs.twimg.com/profile_images/1447671101530775553/PpbiCpy7_400x400.jpg"
+          }
+          className=" rounded-full"
+          layout="fill"
+          objectFit="cover"
+          alt="img"
+        />
+      </div>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[13px]">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
     </div>
   );
 }
