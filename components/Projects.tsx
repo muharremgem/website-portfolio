93ec66e234c1a6f9f879d38ff5e59ca7f984236a
muharrem gem <muharremgem28@gmail.com>
2022-12-14 15:51:20 +0300

    sanity

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,10 @@ const Projects = (props: Props) => {
         Projects
       </h3>
 
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 ">
+      <div
+        className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 
+       scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+      >
         {projects.map((project, i) => (
           <div
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
@@ -10,7 +11,10 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgba(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div
+      className="bg-[rgba(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll 
+     overflow-x-hidden z-0 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+    >
       <Head>
         <title>Muharrem GEM Portfolio</title>
       </Head>
@@ -41,6 +45,17 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+      <Link href="hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://lh3.googleusercontent.com/ogw/AOh-ky2NxnN8LwmKjbz2N3OmnUwIU2IELpJqQD-OOCcQFg=s64-c-mo"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
