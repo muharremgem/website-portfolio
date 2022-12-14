514cbaa86022345b4df09f35bda58920947dc2e9
muharrem gem <muharremgem28@gmail.com>
2022-12-14 14:52:23 +0300

    contacctme

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function ContactMe({}: Props) {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:info@muharremgem?subject=${formData.subject}&body=${formData.message} (${formData.email})`;
+  };
   return (
     <div
       className="h-screen flex relative text-center flex-col md:text-left md:flex-row 
@@ -16,9 +30,60 @@ function ContactMe({}: Props) {
           I have got just what you need.
           <span className="underline decoration-[#F7AB0A]/50 ">Lets Talk</span>
         </h4>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+9054640101..</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">info@muharremgem.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl"> 34 Developer 28</p>
+          </div>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
+          </div>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
 }
-
 export default ContactMe;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,6 +26,7 @@ const Projects = (props: Props) => {
           <div
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
           justify-center p-20 md:p-44 h-screen"
+            key={i}
           >
             <motion.img
               initial={{
