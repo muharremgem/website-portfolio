6ca7a1aa542c7b8c689eeca26e1f0214a596b062
muharrem gem <muharremgem28@gmail.com>
2022-12-14 09:01:23 +0300

    workexperıence

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -4,7 +4,7 @@ import ExperienceCard from "./ExperienceCard";
 
 type Props = {};
 
-export default function WorkExperience({}: Props) {
+function WorkExperience({}: Props) {
   return (
     <motion.div
       initial={{
@@ -14,7 +14,8 @@ export default function WorkExperience({}: Props) {
         duration: 1.5,
       }}
       whileInView={{ opacity: 1 }}
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row
+       max-w-full px-10 justify-evenly mx-auto items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px]  text-gray-500 text-2xl">
         Experience
@@ -29,3 +30,5 @@ export default function WorkExperience({}: Props) {
     </motion.div>
   );
 }
+
+export default WorkExperience;
