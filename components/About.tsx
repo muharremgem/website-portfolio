ad496df4a7057c63e6225b747fd8aa7897e360e1
muharrem gem <muharremgem28@gmail.com>
2022-12-16 11:57:46 +0300

    responsıve

--- components/About.tsx
+++ components/About.tsx
@@ -20,7 +20,7 @@ export default function About({ pageInfo }: Props) {
       className="h-screen flex flex-col relative text-center md:text-left 
       md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px]  text-gray-500 text-2xl mm:text-hidden">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl mm:hidden md:block">
         About
       </h3>
       <motion.img
@@ -33,14 +33,14 @@ export default function About({ pageInfo }: Props) {
         }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        className="object-cover rounded-full md:rounded-lg -mb-20 md:mb-0 flex-shrink-0 w-56 h-56   md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] relative"
+        className="object-cover rounded-full md:rounded-lg -mb-20 md:mb-0 flex-shrink-0 w-56 h-56   
+         md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] mt-[4rem]"
         src={urlFor(pageInfo?.profilePic).url()}
         alt="aimg"
       />
-      <div className="space-y-10 px-0 md:px-10 ">
-        <h4 className="text-4xl font-semibold">
-          Here is a
-          <span className="underline decoration-[#F7AB0A]/50"> little </span>
+      <div className="space-y-3 px-0 md:px-10  md:flex md:flex-col md:gap-6">
+        <h4 className="text-4xl font-semibold mm:text-[25px] mm:mt-[5rem] md:text-4xl">
+          Here is a<span className="text-green-700"> little </span>
           background
         </h4>
         <p className="text-sm">{pageInfo?.backgroundInformation}</p>
