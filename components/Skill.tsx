c2c2622b5a5e47f5911a60ce02e7e90731aaf32f
muharrem gem <muharremgem28@gmail.com>
2022-12-17 13:36:10 +0300

    skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,6 +21,7 @@ function Skill({ skill, directionLeft }: Props) {
         className="rounded-full border border-gray-500 object-cover w-24 h-24
          xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out  md:w-28 md:h-28 "
       />
+
       <div
         className="absolute opacity-0 group-hover:opacity-80 transition duration-300 
       ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 "
