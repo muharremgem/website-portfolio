45906a465165bc1a7985386f9fb9f624c3220504
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2023-01-01 01:05:43 +0300

    2023

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -22,7 +22,6 @@ const Projects = ({ projects }: Props) => {
       <h3 className="absolute top-24 uppercase tracking-[20px]  text-gray-500 text-2xl">
         Projects
       </h3>
-
       <div
         className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 
          scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
